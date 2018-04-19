9063972c8c6245216c73192c054444392a18beeb
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-04-19 19:57:33 -0400

    intial Vidly folder upload

---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+    }
+    // /movies/random - we need to create a movie controller with an action called random
+}
