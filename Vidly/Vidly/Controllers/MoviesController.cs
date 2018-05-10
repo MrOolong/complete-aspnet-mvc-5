8a179bb75849744c82b818b0d884ad84b0067426
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-05-10 18:29:35 -0400

    starting convention based routes

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -15,10 +15,6 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
 
         public ActionResult Edit(int id)
@@ -36,6 +32,10 @@ namespace Vidly.Controllers
                 sortBy = "Name";
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        public ActionResult ByReleaseDate()
+        {
+            return View();
         }
     }
 }
